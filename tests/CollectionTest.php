1f8fcb9130de896333eb44797a13ca353c0b07cb
Adam Nicholson <adamnicholson10@gmail.com>
2015-01-08 15:34:24 +0000

    Added basic test for Collection

---
+++ tests/CollectionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Flatbase;
+
+class CollectionTest extends FlatbaseTestCase
+{
+    public function testInstance()
+    {
+        $this->assertTrue(new Collection() instanceof Collection);
+    }
+
+    public function testArrayAccess()
+    {
+        $items = ['one', 'two', 'three'];
+        $collection = new Collection($items);
+        $this->assertEquals($items[0], $collection[0]);
+        $this->assertEquals($items[1], $collection[1]);
+        $this->assertEquals($items[2], $collection[2]);
+        $this->assertEquals(count($collection), 3);
+    }
+}
