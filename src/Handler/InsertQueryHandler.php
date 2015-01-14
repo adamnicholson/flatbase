58247d84bc90539c233f55659e11253485a76780
Adam Nicholson <adamnicholson10@gmail.com>
2015-01-14 17:08:23 +0000

    Refactored insert queries to not require deserializing of the existing file. Improves speed of writes by around 10x

--- src/Handler/InsertQueryHandler.php
+++ src/Handler/InsertQueryHandler.php
@@ -13,16 +13,39 @@ class InsertQueryHandler extends QueryHandler
         // Validate the query
         $this->validateQuery($query);
 
-        // Read the existing data
-        $readQuery = new ReadQuery();
-        $readQuery->setCollection($query->getCollection());
-        $existing = $this->flatbase->execute($readQuery);
+        $file = $this->flatbase->getStorage()->storageDir . '/' . $query->getCollection();
+        $serialized = file_get_contents($file);
+        $this->appendToSerializedString($serialized, $query->getValues());
 
-        // Append the new item to it
-        $existing->append($query->getValues());
+        file_put_contents($file, $serialized);
+    }
 
-        // Write back to the storage
-        $this->write($query->getCollection(), (array) $existing);
+    protected function appendToSerializedString(&$serialized, $item)
+    {
+        // Calculate the number of items in the serialized string
+        $colonsEncountered = 0;
+        $count = '';
+        for ($i=0; $i<=10; $i++) {
+            if ($colonsEncountered === 2) {
+                break;
+            }
+            if ($colonsEncountered === 1 && $serialized[$i] !== ':') {
+                $count .= $serialized[$i];
+            }
+            if ($serialized[$i] === ':') {
+                $colonsEncountered++;
+            }
+        }
+
+        // Add the new item to the end of the serialized string, just before the closing brace
+        $serialized = substr_replace($serialized, ('i:' . $count . ';' . serialize($item) . ''), (strlen($serialized) - 1), 0);
+
+        // Update the item count at the start of the serialized string
+        $oldDecleration = 'a:' . $count . ':';
+        $newDecleration = 'a:' . ($count+1) . ':';
+        $serialized = substr_replace($serialized, $newDecleration, 0, strlen($oldDecleration));
+
+        return $serialized;
     }
 
     protected function validateQuery(Query $query)
