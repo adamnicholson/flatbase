75061d0b006e1830483dab9b4e90aa4a2970cd86
Adam Nicholson <adamnicholson10@gmail.com>
2015-03-21 11:53:17 +0000

    Improved read command tests

--- tests/Console/Commands/ReadCommandTest.php
+++ tests/Console/Commands/ReadCommandTest.php
@@ -6,6 +6,7 @@ use Flatbase\Collection;
 use Flatbase\Console\Dumper;
 use Flatbase\FlatbaseTestCase;
 use Flatbase\Query\ReadQuery;
+use Prophecy\Argument;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
@@ -23,19 +24,43 @@ class ReadCommandTest extends FlatbaseTestCase
 
     public function testReadUsesCorrectCollection()
     {
+        $this->runReadCommandTest(['collection' => 'users'], function(ReadQuery $query) {
+                $this->assertEquals($query->getCollection(), 'users');
+                return true;
+        });
+    }
+
+    public function testReadWheres()
+    {
+        $this->runReadCommandTest(['collection' => 'users', '--where' => ['foo,==,bar']], function(ReadQuery $query) {
+                $this->assertEquals($query->getCollection(), 'users');
+                $wheres = $query->getConditions();
+                $this->assertEquals(count($wheres), 1);
+                $this->assertEquals($wheres[0], ['foo', '==', 'bar']);
+                return true;
+        });
+    }
+
+    protected function runReadCommandTest(array $input, callable $executeArgumentExpectation)
+    {
+        // Create the command
         $command = new ReadCommand(new VarCloner(), new Dumper());
 
+        // Attach a Flatbase double to the command
         $flatbase = $this->prophesize('Flatbase\Flatbase');
         $command->setFlatbaseFactory(function($storageDir) use ($flatbase) {
-            return $flatbase->reveal();
-        });
+                return $flatbase->reveal();
+            });
 
-        $flatbase->read()->willReturn($query = new ReadQuery());
+        // Expect read() to be called and return a ReadQuery. Making sure the ReadQuery uses our Flatbase double
+        $flatbase->read()->shouldBeCalled()->willReturn($query = new ReadQuery());
         $query->setFlatbase($flatbase->reveal());
-        $flatbase->execute($query)->shouldBeCalled()->willReturn(new Collection());
-        $input = new ArrayInput([
-            'collection' => 'users'
-        ]);
+
+        // Define what the query argument should look like when Flatbase::execute() is called
+        $expectedQuery = Argument::that($executeArgumentExpectation);
+
+        $flatbase->execute($expectedQuery)->shouldBeCalled()->willReturn(new Collection());
+        $input = new ArrayInput($input);
 
         $output = $this->prophesize('Symfony\Component\Console\Output\OutputInterface');
         $command->run($input, $output->reveal());
